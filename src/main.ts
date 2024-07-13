9fcaf0fcb746d5ab3ac1c650d4d48f0ece4ef654
mintech-dot <mintech@protonmail.com>
2024-07-13 06:20:13 +0100

    feat: integrate Apollo Client into main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp, provide, h } from "vue";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import App from "./App.vue";
+import { apolloClient } from "./api/ApolloClient";
+import "./style.css";
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+  render: () => h(App),
+});
 
-createApp(App).mount('#app')
+app.mount("#app");
